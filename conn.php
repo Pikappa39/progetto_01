cbc8a471fd800098cac60e08f9b9092aae0a03f4
Pikappa39 <lorenziluca57@gmail.com>
2026-04-13 16:40:26 +0200

    modifche generiche regerger

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO("mysql:host=localhost;dbname=test", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=prgtt_01", "root", "");
     // Attiva modalità di errore eccezioni
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
--- read.php
+++ read.php
@@ -1,3 +1,5 @@
 <?php
-//
+//questo file serve a leggere i dati del db ed evita che un utente non esistente possa accedere alla pagina di modifica dei dati
+
+
 ?>
